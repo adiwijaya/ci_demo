ada0746f4e664c570ba3b891ab12cad7c5238535
Adi Wijaya <adiwijaya@ad.corp.google.com>
2020-01-31 20:10:15 +0800

    Add unittest. can run using python script

--- test_calculator.py
+++ test_calculator.py
@@ -1,15 +1,24 @@
 """
 Unit tests for the calculator library
-# pytest -v --cov
+# python -m unittest test_calculator.py -v
 """
 
 import calculator
+import unittest
 
-
-class TestCalculator:
-
+class TestCalculator(unittest.TestCase):
     def test_addition(self):
-        assert 4 == calculator.add(2, 2)
+        self.assertEqual(4, calculator.add(2, 2))
 
     def test_subtraction(self):
-        assert 2 == calculator.subtract(4, 2)
+        self.assertEqual(2, calculator.subtract(4, 2))
+
+if __name__ == '__main__':
+    """
+    Verbosity level
+    0 (quiet): you just get the total numbers of tests executed and the global result
+    1 (default): you get the same plus a dot for every successful test or a F for every failure
+    2 (verbose): you get the help string of every test and the result
+    """
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestCalculator)
+    unittest.TextTestRunner(verbosity=2).run(suite)
