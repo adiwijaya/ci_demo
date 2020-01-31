fc5f0d5b18bc8a1bfdbe0380e2ba06e9b9a3e330
Adi Wijaya <adiwijaya@ad.corp.google.com>
2020-01-31 19:32:55 +0800

    Add Calculator functions

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-import numpy
+"""
+Calculator library containing basic math operations.
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
